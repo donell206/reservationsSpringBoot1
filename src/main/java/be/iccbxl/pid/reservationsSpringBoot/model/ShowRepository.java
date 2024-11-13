896ced63111f09261f8f1581ddfd67be09234ca5
Donnel Nkurunziza <donnel206@hotmail.com>
2024-11-13 17:17:51 +0100

    Modèle Show +Relation ManyToOne with Location

---
+++ src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/ShowRepository.java
@@ -0,0 +1,10 @@
+package be.iccbxl.pid.reservationsSpringBoot.model;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ShowRepository extends CrudRepository<Show, Long> {
+    Show findBySlug(String slug);
+    Show findByTitle(String title);
+    List<Show> findByLocation(Location location);
+}
