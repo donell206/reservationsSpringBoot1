aa4548d3091f0e709a3d96c1f8ddc9a15fec3380
Donnel Nkurunziza <donnel206@hotmail.com>
2024-11-16 13:52:11 +0100

    Relation ManyToMany between Artist & Type

--- src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Artist.java
+++ src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Artist.java
@@ -5,23 +5,68 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="artists")
 public class Artist {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String firstname;
     private String lastname;
 
+    @ManyToMany(mappedBy = "artists")
+    private List<Type> types = new ArrayList<>();
+
+    protected Artist() {}
+
     public Artist(String firstname, String lastname) {
         this.firstname = firstname;
-
-
-
         this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<Type> getTypes() {
+        return types;
+    }
+
+    public Artist addType(Type type) {
+        if(!this.types.contains(type)) {
+            this.types.add(type);
+            type.addArtist(this);
+        }
+
+        return this;
+    }
+
+    public Artist removeType(Type type) {
+        if(this.types.contains(type)) {
+            this.types.remove(type);
+            type.getArtists().remove(this);
+        }
+
+        return this;
     }
 
     @Override
@@ -29,3 +74,4 @@ public class Artist {
         return firstname + " " + lastname;
     }
 }
+
