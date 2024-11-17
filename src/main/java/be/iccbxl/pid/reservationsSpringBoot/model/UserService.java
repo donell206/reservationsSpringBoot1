d57c59f1b37f5a61b3ccb7ba7d2c7422d347192d
Donnel Nkurunziza <donnel206@hotmail.com>
2024-11-17 16:16:34 +0100

    Added UserRepository & UserService

---
+++ src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/UserService.java
@@ -0,0 +1,41 @@
+package be.iccbxl.pid.reservationsSpringBoot.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+
+        repository.findAll().forEach(users::add);
+
+        return users;
+    }
+
+    public User getUser(String id) {
+        int indice = Integer.parseInt(id);
+
+        return repository.findById(indice);
+    }
+
+    public void addUser(User user) {
+        repository.save(user);
+    }
+
+    public void updateUser(String id, User user) {
+        repository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        Long indice = (long) Integer.parseInt(id);
+
+        repository.deleteById(indice);
+    }
+}
