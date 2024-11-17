40931dfa77ef9506530f6f61f1ab6dd90e6dfebb
Donnel Nkurunziza <donnel206@hotmail.com>
2024-11-17 16:24:37 +0100

    Relation ManyToMany between User & Representation

--- src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Representation.java
+++ src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Representation.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Table(name="representations")
 public class Representation {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     @ManyToOne
@@ -32,40 +32,37 @@ public class Representation {
     @JoinColumn(name="location_id", nullable=true)
     private Location location;
 
+    @ManyToMany
+    @JoinTable(
+            name = "reservations",
+            joinColumns = @JoinColumn(name = "representation_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
+
     public Representation() { }
 
-    public Representation(Show show, LocalDateTime when, Location location) {
-        this.show = show;
-        this.when = when;
-        this.location = location;
+    //…
+
+    public List<User> getUsers() {
+        return users;
     }
 
-    public Show getShow() {
-        return show;
+    public Representation addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRepresentation(this);
+        }
+
+        return this;
     }
 
-    public void setShow(Show show) {
-        this.show = show;
-    }
+    public Representation removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRepresentations().remove(this);
+        }
 
-    public LocalDateTime getWhen() {
-        return when;
-    }
-
-    public void setWhen(LocalDateTime when) {
-        this.when = when;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public Long getId() {
-        return id;
+        return this;
     }
 
     @Override
@@ -75,3 +72,4 @@ public class Representation {
     }
 
 }
+
--- src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/User.java
+++ src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/User.java
@@ -110,6 +110,30 @@ public class User {
         return this;
     }
 
+    public List<Representation> getRepresentations() {
+        return representations;
+    }
+
+    public User addRepresentation(Representation representation) {
+        if(!this.representations.contains(representation)) {
+            this.representations.add(representation);
+            representation.addUser(this);
+        }
+
+        return this;
+    }
+
+    public User removeRepresentation(Representation representation) {
+        if(this.representations.contains(representation)) {
+            this.representations.remove(representation);
+            representation.getUsers().remove(this);
+        }
+
+        return this;
+    }
+
+
+
     @Override
     public String toString() {
         return login + "(" + firstname + " " + lastname + ")";
