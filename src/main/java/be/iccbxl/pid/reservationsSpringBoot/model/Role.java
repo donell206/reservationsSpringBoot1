db660c1094d91d9cdc804fcec1222a7361e0013c
Donnel Nkurunziza <donnel206@hotmail.com>
2024-11-17 16:14:02 +0100

    Relation ManyToMany between User & Role

--- src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Role.java
+++ src/main/java/be/iccbxl/pid/reservationsSpringBoot/model/Role.java
@@ -3,18 +3,27 @@ package be.iccbxl.pid.reservationsSpringBoot.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="roles")
 public class Role {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String role;
 
-    protected Role() {	}
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
+
+    protected Role() { }
 
     public Role(String role) {
-        super();
         this.role = role;
     }
 
@@ -30,9 +39,32 @@ public class Role {
         this.role = role;
     }
 
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public Role addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRole(this);
+        }
+
+        return this;
+    }
+
+    public Role removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRoles().remove(this);
+        }
+
+        return this;
+    }
+
     @Override
     public String toString() {
         return "Role [id=" + id + ", role=" + role + "]";
     }
 
 }
+
